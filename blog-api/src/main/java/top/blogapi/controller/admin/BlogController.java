a9d24d52f3427088ce728685ed4543b69326e1bd
thinhh0x1l <hoangthinhh18.deva@gmail.com>
2025-10-09 23:58:43 +0700

    add the PageHelp pagination for Mybaits

--- blog-api/src/main/java/top/blogapi/controller/admin/BlogController.java
+++ blog-api/src/main/java/top/blogapi/controller/admin/BlogController.java
@@ -1,10 +1,14 @@
 package top.blogapi.controller.admin;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.blogapi.entity.Blog;
 import top.blogapi.service.BlogService;
@@ -22,11 +26,15 @@ public class BlogController {
     BlogService blogService;
 
     @GetMapping("/blogs")
-    public Result blogs() {
-        try {
-            List<Blog> blogs = blogService.getBlogList();
+    public Result blogs(@RequestParam(defaultValue = "") String query,
+                        @RequestParam(defaultValue = "") String typeId,
+                        @RequestParam(defaultValue = "1") String pageNum,
+                        @RequestParam(defaultValue = "10") String pageSize) {
+        String orderBy = "create_time desc";
+        try(Page<Object> page = PageHelper.startPage(Integer.parseInt(pageNum), Integer.parseInt(pageSize), orderBy)) {
+            PageInfo<Blog> pageInfo = new PageInfo<>(blogService.getBlogList());
             Map<String, Object> map = new HashMap<>();
-            map.put("blogs", blogs);
+            map.put("blogs", pageInfo);
             return Result.ok("Yêu cầu thành công", map);
         } catch (Exception e) {
             return Result.create(500, "Lỗi ngoại lệ");
