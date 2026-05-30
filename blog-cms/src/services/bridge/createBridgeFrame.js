61fa87ed7c8058c057d7219d1f924d069d0e4286
thinhh0x1l <hoangthinhh18.deva@gmail.com>
2026-05-30 20:34:25 +0700

    update bridge

--- blog-cms/src/services/bridge/createBridgeFrame.js
+++ blog-cms/src/services/bridge/createBridgeFrame.js
@@ -3,7 +3,14 @@ export function createBridgeFrame() {
     return new Promise((resolve) => {
         const iframe = document.createElement("iframe");
         iframe.id = "bridge-frame";
-        iframe.src = `${import.meta.env.VITE_BRIDGE_FE}/bridge.html`;
+        const bridgeUrl = import.meta.env.VITE_BRIDGE_FE;
+
+        iframe.src =
+            `${bridgeUrl}/bridge.html?origin=${
+                encodeURIComponent(location.origin)
+            }`;
+
+        // iframe.src = `${import.meta.env.VITE_BRIDGE_FE}/bridge.html`;
         iframe.style.display = "none";
         iframe.onload = () => {
             resolve();
